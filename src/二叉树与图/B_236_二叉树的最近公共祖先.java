0fdab105970bc30c012f884e335892e963f34aac
adogzZ <15269199005@163.com>
2019-08-20 20:43:08 +0800

    LeetCode_236

--- src/二叉树与图/B_236_二叉树的最近公共祖先.java
+++ src/二叉树与图/B_236_二叉树的最近公共祖先.java
@@ -17,6 +17,9 @@ public class B_236_二叉树的最近公共祖先 {
         }
     }
 
+    //finish表示是否找到了节点，把finish定义成全局变量，否则，当找到指定节点发生回溯时finish又会变为0；
+    int finish = 0;
+
     /**
      * 深度优先搜索二叉树，记录从根节点到两个指定节点的节点路径，两个指定节点的节点路径中
      * 最后出现的相同的节点即为最近公共祖先。
@@ -29,13 +32,13 @@ public class B_236_二叉树的最近公共祖先 {
         //从根节点到p，q俩节点的节点路径
         List<TreeNode> p_path = new ArrayList<>();
         List<TreeNode> q_path = new ArrayList<>();
-        List<TreeNode> path = new ArrayList<>();
-        int finish = 0;
+
         //前序遍历二叉树，记录到p节点的节点路径
-        preOrder(root,p,path,p_path,finish);
+        preOrder(root,p,p_path);
         finish = 0;
         //前序遍历二叉树，记录到q节点的节点路径
-        preOrder(root,q,path,q_path,finish);
+        preOrder(root,q,q_path);
+
         int path_len = 0;
         if (p_path.size() < q_path.size()){
             path_len = p_path.size();
@@ -46,7 +49,7 @@ public class B_236_二叉树的最近公共祖先 {
         TreeNode result = new TreeNode(-1);
         //同时遍历p，q俩节点的节点路径
         for (int i = 0;i < path_len;i ++){
-           if (p_path.get(i).val == q_path.get(i).val){
+           if (p_path.get(i) == q_path.get(i)){
                result = p_path.get(i);
            }
         }
@@ -58,20 +61,21 @@ public class B_236_二叉树的最近公共祖先 {
      * @param node
      * @param search
      * @param path
-     * @param finish：是否找到了节点
      */
-    private void preOrder(TreeNode node, TreeNode search, List<TreeNode> path, List<TreeNode> result, int finish) {
+    private void preOrder(TreeNode node, TreeNode search, List<TreeNode> path) {
         if (node == null || finish == 1){
             return;
         }
-        path.add(new TreeNode(node.val));
+        path.add(node);
         //找到search节点
         if (node.val == search.val){
             finish = 1;
-            result = path;
         }
-        preOrder(node.left,search,path,result,finish);
-        preOrder(node.right,search,path,result,finish);
-        path.remove(path.size() - 1);
+        preOrder(node.left,search,path);
+        preOrder(node.right,search,path);
+        //当找到了节点，不要修改path
+        if (finish == 0){
+            path.remove(path.size() - 1);
+        }
     }
 }
